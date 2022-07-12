ba450ca8e1e79b700b611100f5fde3683fa40f2e
Tom Ash <54542292+cwikio@users.noreply.github.com>
2022-07-11 23:12:07 -0400

    Add your FB details to make it work

---
+++ firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "xxx",
+    authDomain: "xxx",
+    projectId: "xxx",
+    storageBucket: "xxx",
+    messagingSenderId: "xxx",
+    appId: "xxx"
+};
+
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
+export const db = getFirestore(app);
